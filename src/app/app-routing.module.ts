3c04be9eb8d1c886a5d0cb4b48e24a2361a0e083
Miguel Ramos <return.infox@gmail.com>
2024-08-22 10:18:57 -0400

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
 
 // http://localhost:4200/
 // http://localhost:4200/home
 // http://localhost:4200/about
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'directives', component: DirectivesComponent },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
